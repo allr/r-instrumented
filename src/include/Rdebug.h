d242109d452bcc499f626d03175351bfcd2e90f2
Markus Kuenne <kuenne@ls12pc11.cs.tu-dortmund.de>
2013-11-13 15:58:25 +0100

    more debug - trying to catch longjumps

--- src/include/Rdebug.h
+++ src/include/Rdebug.h
@@ -1,4 +1,7 @@
 #ifndef _RDEBUG_H
+#define _RDEBUG_H
+
+#include <setjmp.h> // so jumpbuf is known
 
 #define ENABLE_SCOPING_DEBUG
 //#undef ENABLE_SCOPING_DEBUG
@@ -13,7 +16,10 @@
   #define DEBUGSCOPE_READFILE(fileName) do {} while(0)
   #define DEBUGSCOPE_START(scopeName) do {} while(0)
   #define DEBUGSCOPE_END(scopeName) do {} while(0)
-  #define DEBUGSCOPE_PRINT(outText) do {} while (0)
+  #define DEBUGSCOPE_PRINT(outText,...) do {} while (0)
+  #define DEBUGSCOPE_PRINTSTACK() do {} while (0)
+  #define DEBUGSCOPE_SAVEJUMP(jumpInfo) do {} while (0)
+  #define DEBUGSCOPE_LOADJUMP(jumpInfo) do {} while (0)
   
 #endif
 #ifdef ENABLE_SCOPING_DEBUG
@@ -32,14 +38,30 @@
     struct scopes_linlist_t* next;
   } activeScopesLinList;
   
+  typedef struct jumpInfos_linlist_t{
+    jmp_buf jumpInfo; // map index
+    debugScope* targetScope;
+    struct jumpInfos_linlist_t* next;
+  } jumpInfos_linlist;
+  
   #define DEBUGSCOPE_ISACTIVE(scopeName) (debugScope_isActive(scopeName))
   #define DEBUGSCOPE_ACTIVATE(scopeName){ debugScope_activate(scopeName); }
   #define DEBUGSCOPE_READFILE(fileName){ debugScope_readFile(fileName); }
   #define DEBUGSCOPE_START(scopeName) { debugScope_start(scopeName); }
   #define DEBUGSCOPE_END(scopeName) { debugScope_end(scopeName); }
-  #define DEBUGSCOPE_PRINT(outText) { debugScope_print(outText); }
-
-  
+  #define DEBUGSCOPE_PRINT(outText,...) { debugScope_print(outText,##__VA_ARGS__); }
+  /*
+   * Note: __VA_ARGS__ allows the macro to accept a variadic number of arguments.
+   * However, at least one must be provided (in addition to outText).
+   * The token paste operator '##' has to be used to allow
+   * those additional arguments to be omitted.
+   *
+   * For additional information in this matter (at least for the gcc-cpp):
+   * http://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
+   */
+  #define DEBUGSCOPE_PRINTSTACK() { debugScope_printStack(); }
+  #define DEBUGSCOPE_SAVEJUMP(jumpInfo) { debugScope_saveJump(jumpInfo); }
+  #define DEBUGSCOPE_LOADJUMP(jumpInfo) { debugScope_loadJump(jumpInfo); }
   
   //! Marks the given Scope as active - as in "print info from it"
   void debugScope_activate(char* scopeName);
@@ -52,7 +74,12 @@
   
   void debugScope_start(char* scopeName);  
   void debugScope_end(char* scopeName);
-  void debugScope_print(char* output);  
+  void debugScope_print(char* output,...);
+  
+  void debugScope_printStack();
+
+  void debugScope_loadJump(jmp_buf jumpInfo);  
+  void debugScope_saveJump(jmp_buf jumpInfo);  
 #endif
 
   
--- src/main/Rdebug.c
+++ src/main/Rdebug.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h> // for isspace(int)
-
+#include <stdarg.h> // for va_start, va_end
 
 #include <Rdebug.h>
 
+#ifdef ENABLE_SCOPING_DEBUG
+
 static debugScope* currentScope = (debugScope*)NULL;
 static activeScopesLinList* activeScopes = (activeScopesLinList*)NULL;
-
+static jumpInfos_linlist* jumpInfos = (jumpInfos_linlist*)NULL;
 
 void debugScope_activate(char* scopeName){
   activeScopesLinList* newScope = malloc(sizeof(activeScopesLinList));
@@ -30,7 +32,7 @@ int debugScope_isActive(char* scopeName){
     }
   }
   // not found (finally)
-  return (1!=1);
+  return (0!=0);
 }
   
   
@@ -70,9 +72,7 @@ void debugScope_readFile(char* fileName){
         }
       
       if(lineLength>0){ // finally: activate
-        DEBUGSCOPE_PRINT("Activating Scope '");
-        DEBUGSCOPE_PRINT(extractedLine);
-        DEBUGSCOPE_PRINT("'\n");
+        DEBUGSCOPE_PRINT("Activating Scope '%s'\n",extractedLine);
         
         debugScope_activate(extractedLine);
       }
@@ -84,6 +84,19 @@ void debugScope_readFile(char* fileName){
   DEBUGSCOPE_END("debugScope_readFile");
   
 }
+
+#define JUSTJUMP_TEST
+//#undef JUSTJUMP_TEST
+/*
+ * The Justjump-Test is a limited debug mode which is only used to check
+ * whether longjumps from the R interpreter can be correctly detected.
+ *
+ * It maps the standard scope-start and scope-end methods to nothing
+ * and only stores the jumpInfo in it's list (instead of full info).
+ */
+
+
+#ifndef JUSTJUMP_TEST
 
 void debugScope_start(char* scopeName){
   debugScope* newScope = malloc(sizeof(debugScope));
@@ -116,7 +129,7 @@ void debugScope_end(char* scopeName){
       printf(
         "Trying to exit scope %s but current Scope is %s - this should not happen!\n",
         scopeName, 
-        currentScope
+        currentScope->scopeName
         );
     }else{ // scopenames match
       if (currentScope->enabled){
@@ -134,14 +147,67 @@ void debugScope_end(char* scopeName){
     }
   }
 }
-      
-  
-  
 
-void debugScope_print(char* output){
+void debugScope_saveJump(jmp_buf givenJumpInfo){
+  if(NULL != currentScope){ // safety check
+    jumpInfos_linlist* newJumpInfo = malloc(sizeof(jumpInfos_linlist));
+    if(NULL == newJumpInfo){
+      printf(
+        "ERROR: malloc failed for jumpinfos in %s\n",
+        currentScope->scopeName
+        );
+      return;
+    }
+    memcpy(newJumpInfo->jumpInfo,givenJumpInfo,sizeof(jmp_buf));
+    newJumpInfo->targetScope = currentScope;
+    newJumpInfo->next = jumpInfos;
+    
+    jumpInfos = newJumpInfo;
+    //debugScope_print(
+    printf(
+      "# Saved a jump target in %s\n",
+      currentScope->scopeName
+      );
+  }else{ // NULL
+    printf("Current Scope is NULL - this should not happen!\n");
+  }
+}  
+  
+void debugScope_loadJump(jmp_buf givenJumpInfo){
+  if(NULL != currentScope){ // safety check
+    //unsigned int countedJumpInfos = 0;
+    jumpInfos_linlist* jumpInfoWalker = jumpInfos;
+    while(1==1){ // forever - until something happens
+      if(NULL == jumpInfoWalker){ // iterated complete table - but not found
+        printf("ERROR: loadJump but target unknown\n");
+        //printf("ERROR: loadJump but target unknown - %d entrys\n",countedJumpInfos);
+        return;
+      }
+      if(0!=memcmp(jumpInfoWalker->jumpInfo, givenJumpInfo,sizeof(jmp_buf))){// not equal
+        // not found - yet
+        jumpInfoWalker = jumpInfoWalker->next;
+        //countedJumpInfos++;
+        continue;
+      }
+      // found
+      break;
+    }
+    debugScope* targetScope = jumpInfoWalker->targetScope;
+    printf("loadJump - with target %s\n",targetScope->scopeName);
+    // todo: stack correction - remove scopes from linlist
+    
+    // todo: jumpInfo correction - remove jumpInfo(s?) from linlist
+  }else{ // NULL
+    printf("Current Scope is NULL - this should not happen!\n");
+  }
+}  
+void debugScope_print(char* output,...){
   if (NULL != currentScope){ // safety check
     if ((1==1)==currentScope->enabled){
-      printf(output);
+      va_list argumentpointer;
+      va_start(argumentpointer,output);
+      vprintf(output,argumentpointer);
+      va_end(argumentpointer);
     }else{
       /* debug scope not enabled - do not print */
     }
@@ -149,7 +215,128 @@ void debugScope_print(char* output){
     printf("Current Scope is NULL - this should not happen!\n");
   }
 }   
+
+#endif // JUSTJUMP_TEST
+
+#ifdef JUSTJUMP_TEST 
+void debugScope_start(char* scopeName){
+  /* do nothing */
+}
+void debugScope_end(char* scopeName){
+  /* do nothing */
+}
+void debugScope_print(char* output,...){
+  /* do nothing */
+}
+
+#define PRINT_JUMP_INFO
+//#undef PRINT_JUMP_INFO
+void printJumpInfo(jmp_buf givenJumpInfo){
+  unsigned char* jumpInfoBin = (unsigned char*) givenJumpInfo;
+  #ifdef PRINT_JUMP_INFO
+  { 
+    unsigned int jumpInfoIterator;
+    for (
+      jumpInfoIterator  = 0;
+      jumpInfoIterator  < sizeof(jumpInfos_linlist);
+      jumpInfoIterator++
+      )
+    {
+      printf("%02X",jumpInfoBin[jumpInfoIterator]);
+    }
+  }
+  #endif // PRINT_JUMP_INFO
+}
+
+void debugScope_saveJump(jmp_buf givenJumpInfo){
+  jumpInfos_linlist* newJumpInfo = malloc(sizeof(jumpInfos_linlist));
+  if(NULL == newJumpInfo){
+    printf(
+      "ERROR: malloc failed for jumpinfos\n",
+      currentScope->scopeName
+      );
+    return;
+  } // null from malloc - failed
+  
+  // copy necessary information
+  memcpy(newJumpInfo->jumpInfo,givenJumpInfo,sizeof(jmp_buf));
+  
+  // push to stack
+  newJumpInfo->next = jumpInfos;
+  jumpInfos = newJumpInfo;
+  printf("# Saved a jump target:");
+  printJumpInfo(givenJumpInfo);
+    
+  printf("\n");
+}  
+void debugScope_loadJump(jmp_buf givenJumpInfo){
+  printf("# try to loadJump");
+  printJumpInfo(givenJumpInfo);
+  printf("\n");
+  unsigned int countedJumpInfos = 0;
+  jumpInfos_linlist* jumpInfoWalker = jumpInfos;
+  while(1==1){ // forever - until something happens
+    if(NULL == jumpInfoWalker){ // iterated complete table - but not found
+      //printf("ERROR: loadJump - target unknown\n");
+      printf("ERROR: loadJump but target unknown - %d entrys\n",countedJumpInfos);
+      return;
+    }
+    if(0!=memcmp(jumpInfoWalker->jumpInfo, givenJumpInfo,sizeof(jmp_buf))){// not equal
+      // not found - yet
+      jumpInfoWalker = jumpInfoWalker->next;
+      countedJumpInfos++;
+      continue;
+    }
+    // found
+    break;
+  }
+  printf("loadJump - target found\n");
+  printf("loadJump - target found, %d entrys iterated\n",countedJumpInfos);
+}  
+
+#endif // JUSTJUMP_TEST
+
+/*
+ * Reflekting on the jumpinfo-table, when elements should be deleted.
+ *
+ * Technically, this jumpinfo-table is bound to grow over an Interpreter run.
+ * It might sound like a good idea to delete "no longer needed"-entries.
+ *
+ * Unfortunately, there is no safe way to determine which entries are
+ * no longer needed.
+ *
+ * We might iterate the R environments whether any of them still have the 
+ * jumpinfo (jmp_buf) in question but this would mean implementing some
+ * kind of garbage collection. 
+ * We might delete entries when they are loaded - but it is possible to longjump
+ * to the same jmp_buf more than once.
+ * Even if we could do a complete collection of all jmp_buf in memory, there
+ * is nothing preventing a programmer to buffer some of these on disk and re-load
+ * them when needed. 
+ *
+ * So, although it is obvious that some jmp_buf will never be used, again 
+ * (e.g. the setup_Rmainloop in main.c overwrites R_Toplevel.cjmpbuf later), 
+ * it can not be determined from just the debugscope-point of view. 
+ * 
+ * We could, however, implement some kind of "DEBUGSCOPE_CLEARJUMP" so the
+ * programmer can mark jmp_bufs that will never be needed, again.
+ *
+ * Also, the same jmp_buf should not be needed more than once in the table.
+ * However, killing dupes might be less efficient than just accepting them - 
+ * newer entries for the same jmp_buf will be found first, as we are
+ * using a stack as map structure.
+ */
+  
+void debugScope_printStack(){
+  printf("--- START Debug Scope Stack ---\n");
+  debugScope* scopeIterator = currentScope;
+  while(NULL != scopeIterator){
+    printf("* %s\n",scopeIterator->scopeName);
+    scopeIterator = scopeIterator->parent;
+  }
+  printf("--- END Debug Scope Stack ---\n");
+}
   
 
-
+#endif // ENABLE_SCOPING_DEBUG
 
